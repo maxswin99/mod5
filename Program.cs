5b0d5db8ac649a21059841a6b2e7c663caafaa9c
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-20 10:41:52 +0700

    menambahkan class PemrosesData dengan method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design.Serialization;
+using System.Globalization;
+
+
+public class Program
+{
+    class PemrosesanData
+    {
+        public int DapatkanNilaiTerbesar<T>(T input1, T input2, T input3)
+        {
+            dynamic nilai1 = input1;
+            dynamic nilai2 = input2;
+            dynamic nilai3 = input3;
+            dynamic nilaiTerbesar = input1;
+            if (nilai1 > input2)
+            {
+                nilaiTerbesar = input1;
+            }
+            else if (nilai2 > input1 && nilai2 > input3)
+            {
+                nilaiTerbesar = input2;
+            }
+            else
+            {
+                nilaiTerbesar = input3;
+            }
+            return nilaiTerbesar;
+        }
+
+    }
+    public static void Main()
+    {
+        PemrosesanData pemrosesanData = new PemrosesanData();
+        int x1, x2, x3;
+        System.Console.WriteLine("Masukkan nilai 1: ");
+        x1 = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine("Masukkan nilai 2: ");
+        x2 = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine("Masukkan nilai 3: ");
+        x3 = int.Parse(System.Console.ReadLine());
+
+        int nilaiTerbesar = pemrosesanData.DapatkanNilaiTerbesar(10, 20, 30);
+        System.Console.WriteLine("Nilai terbesar adalah: " + nilaiTerbesar);
+    }
+}
